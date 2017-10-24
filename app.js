3698b18fe74ae77492f94fa0791cbf06c8cb5da1
cmccormack <mccormack.christopher@gmail.com>
2017-10-24 11:30:15 -0700

    Added error handling and 404, completed api request work

--- app.js
+++ app.js
@@ -3,20 +3,38 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(function(req, res, next) {
+let ip, os, lang
+
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+
+  if (req.headers['x-forwarded-for']){
+    ip = req.headers['x-forwarded-for'].split(',')[0]
+  } else {
+    ip = req.connection.remoteAddress
+  }
+  os = req.headers['user-agent'].match(/\([^\)]*\)/)[0].slice(1, -1)
+  lang = req.headers['accept-language'].split(',')[0]
+
   next();
 })
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
+  res.send(JSON.stringify({ipaddress: ip, language: lang, software: os}))
+})
 
-  let ip = req.connection.remoteAddress
-  if (req.headers['x-forwarded-for']){
-    ip = req.headers['x-forwarded-for'].split(',')[0]
-  }
+// Error Handler
+app.use((err, req, res, next) => {
+  console.error(err.message)
+  console.error(err.stack)
+  res.status(500)
+  res.send(`Error: ${err.message}`)
+})
 
-  res.send(ip)
+// All Middleware functions and routes exhausted, respond with 404
+app.use(function (req, res, next) {
+  res.status(404).send("404 Page Not Found")
 })
 
 const server = app.listen(port, () => {
