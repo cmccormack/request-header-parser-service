2803609d064c3f5940cbd5852faad742c4f2deaa
cmccormack <mccormack.christopher@gmail.com>
2017-10-24 09:40:08 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# request-header-parser-service
+# Timestamp Microservice
 Service that returns a JSON object containing IP address, language, and OS version from the user's browser.
+
+**Objective:** Build a full stack JavaScript app that is functionally similar to [this](https://cryptic-ridge-9197.herokuapp.com/api/whoami/).
+
+Fulfill the below user stories:
+* User Story: I can get the IP address, language and operating system for my browser.
---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next();
+})
+
+app.get('/', (req, res) => {
+  for (val in req){
+    if (typeof req[val] === 'string'){
+      console.log(`${val}: ${req[val]}`)
+    }
+  }
+  console.log(req.headers)
+  console.log(req.accepts())
+  res.send('Woo Hoo!')
+})
+
+const server = app.listen(port, () => {
+  console.log(`Server started on port ${port}.`)
+})
